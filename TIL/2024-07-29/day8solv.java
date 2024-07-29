efa067fe2900ac27b4b6ebfa60c97b22e7d1e127
hayeon <98332510+gelong25@users.noreply.github.com>
2024-07-29 21:41:13 +0900

    주석 추가

--- TIL/2024-07-29/day8solv.java
+++ TIL/2024-07-29/day8solv.java
@@ -3,28 +3,30 @@ import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         
-        boolean answer = true;
+        boolean answer = true; // 정답으로 반환할 변수 answer, 초기값은 true로 설정 
 
-        Stack<Character> stack = new Stack<>();
-        
+        Stack<Character> stack = new Stack<>();   // 문자 저장을 위한 스택 선언 
+
+        // 주어진 문자열을 순회하며 문자를 검사하는 반복문 
         for(int i = 0; i<s.length(); i++) {
             char ch = s.charAt(i);
             
-            if(ch == '(') {
-                stack.push(ch);
+            if(ch == '(') {                    // 열린 괄호를 만나 스택에 추가 
+                stack.push(ch);        
             } 
             
-            else if(ch == ')') {
-                if(stack.isEmpty()) {
+            else if(ch == ')') {              // 닫힌 괄호를 만난 경우 
+                if(stack.isEmpty()) {         // 스택이 비어있으면 유효하지 않으므로 false 반환 
                     return false; 
             } 
-            else {
+            else {                            // 스택이 비어있지 않은 경우, 스택의 데이터(열린괄) 제거 
                 stack.pop(); 
             }
         }
     }
-        if (!stack.isEmpty()) {
-            answer = false; 
+        // 유효성 검사 
+        if (!stack.isEmpty()) {              // 스택이 비어있지 않은 경우 (열린괄호와 닫힌 괄호의 개수가 다른 경우) 유효하지 않으므로 
+            answer = false;                  // false 반환 
         }
     
 
@@ -42,44 +44,44 @@ import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         
-        // 초기 정답을 true로 설정
+        // 정답으로 반환할 변수 answer, 초기값은 true로 설정 
         boolean answer = true;
 
         // 스택을 사용하여 문자열의 괄호를 저장
         Stack<Character> stack = new Stack<>();
         
-        // 주어진 문자열을 한 문자씩 스택에 푸시
+        // 주어진 문자열을 한 문자씩 스택에 추가 
         for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i); 
             stack.push(ch);
         }
 
-        // 닫는 괄호와 여는 괄호의 개수를 세기 위한 변수 초기화
+        // 닫힌 괄호와 열린 괄호의 개수를 세기 위한 변수 초기화
         int cnt_close = 0; 
         int cnt_open = 0; 
         
         // 스택이 비어있지 않은 동안 루프 실행
         while(!stack.isEmpty()) {
-            // 스택에서 문자를 하나씩 팝
+            // 스택에서 문자를 하나씩 제거
             char c = stack.pop();
             
-            // 팝된 문자가 닫는 괄호인 경우 cnt_close 증가
+            // 제거된 문자가 닫힌 괄호인 경우 cnt_close 증가
             if(c == ')' ) {
                 cnt_close++;
             }
-            // 팝된 문자가 여는 괄호인 경우
+            // 제거된 문자가 열린 괄호인 경우
             else if (c == '(' ) {
                 if(cnt_close > 0) {
-                    // 만약 닫는 괄호가 이전에 있었다면 cnt_close 감소
+                    // 만약 닫힌 괄호가 이전에 있었다면 cnt_close 감소
                     cnt_close--;
                 } else {
-                    // 닫는 괄호가 없다면 cnt_open 증가
+                    // 닫힌 괄호가 없다면 cnt_open 증가
                     cnt_open++; 
                 }
             }
         }
         
-        // 여는 괄호나 닫는 괄호의 개수가 남아있는 경우 정답을 false로 설정
+        // 열린 괄호나 닫힌 괄호의 개수가 남아있는 경우 정답을 false로 설정
         if(cnt_open != 0 || cnt_close != 0) {
             answer = false; 
         }
