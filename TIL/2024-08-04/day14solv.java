b5286db712f386181fc6d668e0d5c7898d1a2956
hayeon <98332510+gelong25@users.noreply.github.com>
2024-08-04 21:47:54 +0900

    14일차 솔루션

---
+++ TIL/2024-08-04/day14solv.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        
+        if(root == null) {        // 루트 노드가 null이면 대칭이므로 true 반환 
+            return true; 
+        }
+        return isMirror(root.left, root.right);  // 루트의 왼쪽과 오른쪽 서브트리가 대칭인지 확인하기 위한 재귀 함수 호출 
+
+    }
+
+    public boolean isMirror(TreeNode left, TreeNode right) {
+        if(left == null && right == null) {            // 왼쪽과 오른쪽 서브트리의 루트가 둘 다 null이면 대칭이므로 true 반환 
+            return true;
+        } else if (left == null || right == null) {    // 왼쪽 또는 오른쪽 서브트리 중 하나만 null인 경우 대칭이 아니므로 false 반환  
+            return false; 
+        } else if (left.val != right.val) {            // 왼쪽과 오른쪽 노드 값이 다르면 대칭이 아니므로 false 반환 
+            return false; 
+        } 
+        // 왼쪽 노드의 왼쪽 자식과 오른쪽 노드의 오른쪽 자식을 비교하고 
+        // 왼쪽 노드의 오른쪽 자식과 오른쪽 노드의 왼쪽 자식을 비교해 둘 다 대칭인지 확인 
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);  
+    }
+}
